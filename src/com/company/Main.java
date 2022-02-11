1a6bb1091e063a7cf3fbf3568dfd978fae694c09
Melekber <melekberahmed1987@gmail.com>
2022-02-11 19:13:59 +0000

    Forth commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("1st class");
         System.out.println("2nd steps");
         System.out.println("3th change");
+        System.out.println("4th steps");
     }
 }
