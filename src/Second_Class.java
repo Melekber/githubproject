6c632fce2f3849c99fa8c964673873a214b53512
Melekber <melekberahmed1987@gmail.com>
2022-02-11 19:37:42 +0000

    My second class #2

--- src/Second_Class.java
+++ src/Second_Class.java
@@ -2,5 +2,6 @@ public class Second_Class {
     public static void main(String[] args) {
         System.out.println("new page second class");
         System.out.println("how are you");
+        System.out.println("fine");
     }
 }
